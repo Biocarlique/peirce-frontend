8637ff942e0c11f248b25bcd0e38fd4fea9d5c94
Biocarlique <michaeldragon2002g@gmail.com>
2026-05-06 18:56:05 +0300

    feat(root): wrap app with query-client, browser-router and notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient.ts";
+import { Toaster } from "@/components/ui/sonner";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+            <Toaster position="bottom-right" />
+        </QueryClientProvider>
+    </StrictMode>,
+);
